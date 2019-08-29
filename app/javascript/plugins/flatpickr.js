3cffb86e8a68e115ff3f52435f3e3dbee18dc7f7
Sebastian Lipiszko <lipsikko@gmail.com>
2019-08-29 12:32:43 +1000

    Flatpickr debugged

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,11 +1,21 @@
 import flatpickr from "flatpickr"
 import "flatpickr/dist/flatpickr.min.css"
 
-const picker = document.querySelector('.datepicker');
-const disabledDates = picker.form.dataset.disabledDates;
+const init_flatpickr = () => {
+  const picker = document.querySelector('.datepicker');
 
-flatpickr(".datepicker", {
-  minDate: "today",
-  disable: JSON.parse(disabledDates),
+  if (picker) {
+    const disabledDates = picker.form.dataset.disabledDates
 
-});
+    flatpickr(".datepicker.datepicker-search", {
+      minDate: "today",
+    });
+
+    flatpickr(".datepicker.datepicker-booking", {
+      minDate: "today",
+      disable: JSON.parse(disabledDates),
+    });
+  }
+};
+
+init_flatpickr();
